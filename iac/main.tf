ff57c9c8e1308b801fb3aed122514c38ea472cf3
ashutosh5786 <ashutosh5786@outlook.com>
2025-05-21 15:33:14 +0100

    refactor: remove unused subnet and output variables from Terraform configuration

--- iac/main.tf
+++ iac/main.tf
@@ -20,7 +20,6 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "ml-cluster"
   cluster_version = "1.32"
-  subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
   eks_managed_node_groups = {
--- iac/outputs.tf
+++ iac/outputs.tf
@@ -2,11 +2,3 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "region" {
-  value = var.region
-}
-
-output "kubeconfig" {
-  description = "Kubeconfig output"
-  value       = module.eks.kubeconfig_filename
-}
