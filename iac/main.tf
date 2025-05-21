743a12c7e8ada3e0f5c8596a76fda8bdb7f6b598
ashutosh5786 <ashutosh5786@outlook.com>
2025-05-21 15:23:02 +0100

    updated iac

--- iac/main.tf
+++ iac/main.tf
@@ -36,11 +36,3 @@ module "eks" {
     Environment = "test"
   }
 }
-
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "kubeconfig" {
-  value = module.eks.kubeconfig_filename
-}
--- k8s/run.sh
+++ k8s/run.sh
@@ -7,6 +7,7 @@ echo "🚀 [k8s] Applying Kubernetes manifests..."
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
 
+sleep 50
 read -p "➡️  Do you want to port-forward the app to localhost:8000? (y/n): " forward
 if [[ "$forward" == "y" ]]; then
   kubectl port-forward svc/ml-serving 8000:80
